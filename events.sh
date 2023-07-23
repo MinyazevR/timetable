edd41d00bc9b145054a3323f1f57ec624ada186e
Roman <eeuriset8@gmail.com>
2023-07-23 14:45:58 +0300

    add table for google apps script

--- events.sh
+++ events.sh
@@ -1,7 +1,8 @@
 python3 main.py events
-psql -U ntyrreuir -d timetable -c 'COPY (select * from "Event") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/Event.csv'
-psql -U ntyrreuir -d timetable -c 'COPY (select * from "GroupToEvent") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/GroupToEvent.csv'
-psql -U ntyrreuir -d timetable -c 'COPY (select * from "UserToEvent") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/UserToEvent.csv'
+psql -U ntyrreuir -d tt -c 'COPY (select * from "Event") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/Event.csv'
+psql -U ntyrreuir -d tt -c 'COPY (select * from "GroupToEvent") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/GroupToEvent.csv'
+psql -U ntyrreuir -d tt -c 'COPY (select * from "UserToEvent") TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/UserToEvent.csv'
+psql -U ntyrreuir -d tt -c 'COPY (select * from CommonUserAndGroupsEventsInfo) TO STDOUT WITH (FORMAT CSV, HEADER)' -o 'CSV/SpecialEvents.csv'
 git add CSV
 git commit -m "Change tables"
 git push origin main
